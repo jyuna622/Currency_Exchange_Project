6c5f0abf3d80515a1cb124551f4c57119ca9bf53
jyuna622 <jyuna622@gmail.com>
2021-04-22 16:54:26 +0900

    Currency_Exchange_Project_20210422

---
+++ Currency/Monet.java
@@ -0,0 +1,12 @@
+package Currency;
+
+public class Monet {
+	//환전할 종류 선택하기
+	final static int EX_TYPE_EXIT = 0; //종료
+	final static int EX_TYPE_USD = 1; //1.USD
+	final static int EX_TYPE_EUR = 2; //2.EUR
+	final static int EX_TYPE_JPY = 3; //3.JPY
+	final static double EX_RATE_USD = 1127.3; //달러 환율
+	final static double EX_RATE_EUR = 1349.4; //유로 환율
+	final static double EX_RATE_JPY = 10.197; //엔화 혼율
+}
